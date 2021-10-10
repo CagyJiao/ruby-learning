a6ba7c31b25b244580f789d79506d1b321a33dbf
CagyJiao <kaijie.jiao@instructure.com>
2021-10-10 23:49:00 +0200

    using concerns

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    include Visible
     has_many :comments
 
     # add validations for title and body
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_201033) do
+ActiveRecord::Schema.define(version: 2021_10_10_205650) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "aticles", force: :cascade do |t|
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_10_10_201033) do
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
