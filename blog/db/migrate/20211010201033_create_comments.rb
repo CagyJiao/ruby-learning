732e933bb57b72312ef6ac64824d39216df82e2b
CagyJiao <kaijie.jiao@instructure.com>
2021-10-10 22:15:02 +0200

    bin/rails db:migrate to create the database

--- blog/db/migrate/20211010201033_create_comments.rb
+++ blog/db/migrate/20211010201033_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.string :commenter
       t.text :body
+      # it creates an integer column called article_id and foreign key which points to the id column of the articles table 
       t.references :article, null: false, foreign_key: true
 
       t.timestamps
