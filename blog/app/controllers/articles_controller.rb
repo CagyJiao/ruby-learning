eb618289b3b1576b96c0be13d6c2f112bdeec90e
CagyJiao <kaijie.jiao@instructure.com>
2021-10-10 20:45:34 +0200

    strong parameters to filter params

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       # it will cause to make a new request 
@@ -22,5 +22,10 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 
 end
