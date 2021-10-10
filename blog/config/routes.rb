d94b9588fdc7141ccd0943f96c5719db30a7e2d5
CagyJiao <kaijie.jiao@instructure.com>
2021-10-10 20:17:35 +0200

    resources method and link_to

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # set the home page
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 end
