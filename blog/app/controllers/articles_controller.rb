4a31942c75a7f967d506930788e44fc7e2266ba9
CagyJiao <kaijie.jiao@instructure.com>
2021-10-10 20:43:34 +0200

    new and create method for articles, and submit form

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -6,4 +6,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      # it will cause to make a new request 
+      redirect_to @article
+    else
+      # render the specified view for the current request
+      render :new
+    end
+  end
+
 end
