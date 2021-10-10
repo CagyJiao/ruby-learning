1f1b817348a40a5df7268e297cfc00ba63b467bc
CagyJiao <kaijie.jiao@instructure.com>
2021-10-10 21:08:08 +0200

    validation and add new article link

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    # add validations for title and body
+    # presence means the value must contain at least one non-whitespace char
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
 end
